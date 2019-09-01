4ce5ec694deeecdf2a6ca6b2aa5761f6c55bb7f4
xzagta <helmibaran20@gmail.com>
2019-09-01 15:02:16 +0700

    initial commit

--- frontend/src/Belajar.vue
+++ frontend/src/Belajar.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
+    <form @submit.prevent="add">
+        <input type="hidden" v-model="form.id">
+        <input type="text" v-model="form.name">
+        <button type="submit" v-show="!updateSubmit">add</button>  
+        <button type="button" v-show="updateSubmit" @click="update(form)">Update</button> 
+    </form>
     <ul v-for="user in users" :key="user.id">
       <li>
       <span>{{user.name}}</span> &#160;
+      <button @click="edit(user)">Edit</button> ||  <button @click="del(user)">Delete</button>
       </li>
     </ul>
   </div>
@@ -14,7 +21,12 @@ import axios from 'axios'
 export default {
   data(){
     return{
-        users: []
+        form: {
+          id: '',
+          name: ''
+        },
+        users: '',
+        updateSubmit: false
     }
   },
   mounted() {
@@ -23,11 +35,41 @@ export default {
   methods: {
     load(){
         axios.get('http://127.0.0.1:3000/api/users/').then(res => {
-        this.users = res.data //respon dari rest api dimasukan ke users
+        this.users = res.data.Users
       }).catch ((err) => {
         console.log(err);
+        
+      })
+    },
+      add(){
+      axios.post('http://127.0.0.1:3000/api/users/', this.form).then(res => {
+          this.load()
+          this.form.name = ''
+      })
+    },
+    edit(user){ 
+        this.updateSubmit = true
+        this.form.id = user.id 
+        this.form.name = user.name 
+    },
+    update(form){ 
+       return axios.put('http://127.0.0.1:3000/api/users/' + form.id , {name: this.form.name}).then(res => {
+        this.load()
+        this.form.id = ''
+        this.form.name = ''
+        this.updateSubmit = false
+      }).catch((err) => {
+        console.log(err);
+        
+      })
+    },
+    del(user){
+      axios.delete('http://127.0.0.1:3000/api/users/' + user.id).then(res =>{
+          this.load()
+          let index = this.users.indexOf(form.name)
+          this.users.splice(index,1)
       })
     }
-   }
   }
+}
 </script>
